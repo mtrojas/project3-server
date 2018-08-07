be0b4cd518fbdbead4c88e493140380834d3c46c
mtrojas <tererojas.i@gmail.com>
2018-08-07 12:31:57 -0500

    Ready to deploy for demo

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const router  = express.Router();
 const path = require('path');
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-//ready to deploy
-// router.get('/*', (req, res, next) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
+// router.get('/', (req, res, next) => {
+//   res.render('index');
 // });
+
+ready to deploy
+router.get('/*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 module.exports = router;
